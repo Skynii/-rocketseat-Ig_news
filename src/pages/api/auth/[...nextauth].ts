b1c03cb1422a24abfe7c4e689413d50d6c610640
Skynii <82392687+Skynii@users.noreply.github.com>
2021-10-08 11:12:05 -0300

    Add files via upload
    Next-Auth working

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,15 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
 
 export default NextAuth({
-
-    providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            scope: 'read:user'
-        }),
-    ],
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: 'read:user'
+    }),
+    // ...add more providers here
+  ],
 
 })
